975e96d58416381e95e01c62e64bf7392a9c3f15
Abel Sang <abelsang9683@gmail.com>
2018-05-15 17:25:31 -0400

    not finished yet

---
+++ longest_palindromic_substring.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if len(s) == 1:
+            return s
+        for char in s:

ae5d3a758c2969225c7b749bfd39165a5b4f2d49
Yeqi Sang <abelsang9683@gmail.com>
2021-11-14 17:13:23 -0500

    done

---
+++ 704-binary-search.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        left, right = 0, len(nums)-1
+        
+        while left <= right:
+            pivot = (left + right) // 2
+            
+            if nums[pivot] == target:
+                return pivot
+            
+            if target < nums[pivot]:
+                right = pivot - 1
+            
+            else:
+                left = pivot + 1
+        
+        return -1
+        
+        
